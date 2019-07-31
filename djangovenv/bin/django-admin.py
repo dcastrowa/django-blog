a5500d94dcf77d22b85a48d6ec060f7d1bc24d1a
Daniel Castro <daniel.castro@critigen.com>
2019-07-30 21:48:54 -0700

    Complete assignment06

---
+++ djangovenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/danielcastro/Documents/PythonCert/Python230/django-blog/djangovenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
